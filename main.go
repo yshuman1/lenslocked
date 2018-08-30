5760d774fcdd3cd243bc0a879144e11c069a860a
Yasin Shuman <email@yasin.io>
2018-08-29 23:46:22 -0700

    set content type header

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("someone visited our page")
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
