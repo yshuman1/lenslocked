f97156019ef562ecd28e7862a1e402b2eb1a877a
Yasin Shuman <email@yasin.io>
2018-09-09 01:25:26 -0700

    made helper file to parse requests easier

--- controllers/users.go
+++ controllers/users.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
-
 	"lenslocked.com/views"
 )
 
@@ -36,13 +34,8 @@ type SignupForm struct {
 // Create processes signup form to create user account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
