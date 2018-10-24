df6feb33423e4e259aeb55158ea33495226945db
Yasin Shuman <email@yasin.io>
2018-10-23 22:10:29 -0700

    added login form and function

--- controllers/users.go
+++ controllers/users.go
@@ -11,14 +11,16 @@ import (
 // NewUsers creates a newuser controller
 func NewUsers(us *models.UserService) *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-		us:      us,
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
 	}
 }
 
 type Users struct {
-	NewView *views.View
-	us      *models.UserService
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
 }
 
 // New renders signup form
@@ -52,4 +54,19 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintln(w, user)
+}
+
+type LoginForm struct {
+	Email    string `schems:"email"`
+	Password string `schema:"password"`
+}
+
+// Login is used to verify the email and pw and log the user in if theyre correct
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
--- main.go
+++ main.go
@@ -33,8 +33,9 @@ func main() {
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	r.Handle("/login", usersC.LoginView).Methods("GET")
+	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	fmt.Println("server running on port :3000")
-
 	http.ListenAndServe(":3000", r)
 }
 
