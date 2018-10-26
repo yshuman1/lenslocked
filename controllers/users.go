fe7c77fd0901b74b96672d530fa99eba50618984
Yasin Shuman <email@yasin.io>
2018-10-26 15:34:48 -0700

    updated create and login actions to signin user with cookies

--- controllers/users.go
+++ controllers/users.go
@@ -53,7 +53,8 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, user)
+	signIn(w, &user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -83,16 +84,20 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	signIn(w, user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+func signIn(w http.ResponseWriter, user *models.User) {
 	cookie := http.Cookie{
 		Name:  "email",
 		Value: user.Email,
 	}
 
 	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
 }
 
-//used to display cookies set on current user
+// CookieTest is used to display cookies set on current user
 func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("email")
 	if err != nil {
