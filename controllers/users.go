711db50917b4584cfd654d7c798f5284d9f7bcf0
Yasin Shuman <email@yasin.io>
2018-11-01 15:18:30 -0700

    rendered a dynamic alert!!!!

--- controllers/users.go
+++ controllers/users.go
@@ -27,7 +27,15 @@ type Users struct {
 // New renders signup form
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
+	type Alert struct {
+		Level   string
+		Message string
+	}
+	a := Alert{
+		Level:   "success",
+		Message: "Successfully rendered a dynamic alert!  Woohoo!",
+	}
+	if err := u.NewView.Render(w, a); err != nil {
 		panic(err)
 	}
 }
