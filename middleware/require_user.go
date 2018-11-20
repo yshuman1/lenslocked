17454d186c950602a1730519d06854725ee9f113
Yasin Shuman <email@yasin.io>
2018-11-20 00:08:43 -0800

    stopped running user mw when serving static assets

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"lenslocked.com/context"
 	"lenslocked.com/models"
@@ -17,6 +18,13 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		// if user requests staatic image or asset, we dont need to lookup user so we skip doing that...
+		if strings.HasPrefix(path, "/assets/") || strings.HasPrefix(path, "/images/") {
+			next(w, r)
+			return
+		}
+
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
 			next(w, r)
