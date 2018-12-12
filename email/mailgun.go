7f4b58dcd9a00ef48287f0fc780f08477d85a930
Yasin Shuman <email@yasin.io>
2018-12-12 00:17:25 -0800

    17.5.5 done! sending emails for pw reset!

--- email/mailgun.go
+++ email/mailgun.go
@@ -2,9 +2,66 @@ package email
 
 import (
 	"fmt"
+	"net/url"
 
 	mailgun "gopkg.in/mailgun/mailgun-go.v1"
 )
+
+const (
+	welcomeSubject = "Welcome to LensLocked.com!"
+	resetSubject   = "Instructions for resetting your password."
+	resetBaseURL   = "https://www.lenslocked.com/reset"
+)
+
+const welcomeText = `Hi there!
+
+Welcome to LensLocked.com! We really hope you enjoy using
+our application!
+
+Best,
+Jon
+`
+
+const welcomeHTML = `Hi there!<br/>
+<br/>
+Welcome to
+<a href="https://www.lenslocked.com">LensLocked.com</a>! We really hope you enjoy using our application!<br/>
+<br/>
+Best,<br/>
+Jon
+`
+
+const resetTextTmpl = `Hi there!
+
+It appears that you have requested a password reset. If this was you, please follow the link below to update your password:
+
+%s
+
+If you are asked for a token, please use the following value:
+
+%s
+
+If you didn't request a password reset you can safely ignore this email and your account will not be changed.
+
+Best,
+LensLocked Support
+`
+
+const resetHTMLTmpl = `Hi there!<br/>
+<br/>
+It appears that you have requested a password reset. If this was you, please follow the link below to update your password:<br/>
+<br/>
+<a href="%s">%s</a><br/>
+<br/>
+If you are asked for a token, please use the following value:<br/>
+<br/>
+%s<br/>
+<br/>
+If you didn't request a password reset you can safely ignore this email and your account will not be changed.<br/>
+<br/>
+Best,<br/>
+LensLocked Support<br/>
+`
 
 func WithMailgun(domain, apiKey, publicKey string) ClientConfig {
 	return func(c *Client) {
@@ -23,6 +80,7 @@ type ClientConfig func(*Client)
 
 func NewClient(opts ...ClientConfig) *Client {
 	client := Client{
+		// Set a default from email address...
 		from: "support@lenslocked.com",
 	}
 	for _, opt := range opts {
@@ -43,18 +101,21 @@ func (c *Client) Welcome(toName, toEmail string) error {
 	return err
 }
 
+func (c *Client) ResetPw(toEmail, token string) error {
+	v := url.Values{}
+	v.Set("token", token)
+	resetUrl := resetBaseURL + "?" + v.Encode()
+	resetText := fmt.Sprintf(resetTextTmpl, resetUrl, token)
+	message := mailgun.NewMessage(c.from, resetSubject, resetText, toEmail)
+	resetHTML := fmt.Sprintf(resetHTMLTmpl, resetUrl, resetUrl, token)
+	message.SetHtml(resetHTML)
+	_, _, err := c.mg.Send(message)
+	return err
+}
+
 func buildEmail(name, email string) string {
 	if name == "" {
 		return email
 	}
 	return fmt.Sprintf("%s <%s>", name, email)
 }
-
-const welcomeSubject = "welcome to lenslocked.com!"
-const welcomeText = `Hi there!
-Welcome to our website!
-
-Thanks for signing up!
-Yasin`
-
-const welcomeHTML = `Hi there!<br/> Welcome to <a href=“https://lenslocked.com”>Lenslocked.com</a>!<br/>Best,</br>Yasin`
