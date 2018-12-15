2fcdf90614f15033dd484e41a0e2026c18a3c292
Yasin Shuman <email@yasin.io>
2018-12-15 02:46:14 -0800

    begin oauth work for dropbox, 18.4

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
 	"golang.org/x/oauth2"
 	"lenslocked.com/controllers"
@@ -60,13 +61,31 @@ func main() {
 
 	dbxRedirect := func(w http.ResponseWriter, r *http.Request) {
 		state := csrf.Token(r)
+		cookie := http.Cookie{
+			Name:     "oauth_state",
+			Value:    state,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
 		url := dbxOauth.AuthCodeURL(state)
-		fmt.Println(state)
 		http.Redirect(w, r, url, http.StatusFound)
 	}
 	r.HandleFunc("/oauth/dropbox/connect", dbxRedirect)
 	dbxCallback := func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
+		state := r.FormValue("state")
+		cookie, err := r.Cookie("oauth_state")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		} else if cookie == nil || cookie.Value != state {
+			http.Error(w, "invalid state provided", http.StatusBadRequest)
+			return
+		}
+		cookie.Value = ""
+		cookie.Expires = time.Now()
+		http.SetCookie(w, cookie)
+
 		fmt.Fprintln(w, "code: ", r.FormValue("code"), "state: ", r.FormValue("state"))
 	}
 	r.HandleFunc("/oauth/dropbox/callback", dbxCallback)
